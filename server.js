11fa8d393bf37ad28f91191afc2ae8b5d7b97af0
jvillavicencio <Jorge.Villavicencio@dpworldcallao.com.pe>
2021-02-12 22:32:44 -0500

    modular por medio de auth.js y routes.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-const ObjectID = require('mongodb').ObjectID;
+const routes = require('./routes.js'); 
+const auth = require('./auth.js'); 
 
 const app = express();
 app.set('view engine', 'pug');
@@ -15,12 +16,12 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/*app.use(session({
+app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   cookie: { secure: false }
-}));*/
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -28,30 +29,17 @@ app.use(passport.session());
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
 
-  // Be sure to change the title
-  app.route('/').get((req, res) => {
-    // Change the response to render the Pug template
-    res.render(process.cwd() +'/views/pug', {
-      title: 'Connected to Database',
-      message: 'Please login'
-    });
-  });
+  routes(app, myDataBase);
+  auth(app, myDataBase);
 
-  // Serialization and deserialization here...
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-  // Be sure to add this...
 }).catch((e) => {
   app.route('/').get((req, res) => {
-    res.render('pug', { title: e, message: 'Unable to login1' });
+    res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
+
+
+
 // app.listen out here...
 
 app.listen(process.env.PORT || 3000, () => {
